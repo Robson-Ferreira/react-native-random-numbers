b7c8689b346b3c90af4e358fc31d9f4e4e863b19
Robson Durval <robson-durval@hotmail.com>
2018-03-30 14:21:46 -0300

    refactoring and finalizing first app react native

--- App.js
+++ App.js
@@ -6,33 +6,29 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
+  Button,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const generateRandomNumber = () => {
+  var random_number = Math.floor((Math.random()*10));
+  alert(random_number);
+}
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.title}>
+          Clique no botão abaixo para gerar um número randômico.
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button 
+          title= "Gerar número randômico"
+          onPress={generateRandomNumber}
+        />
       </View>
     );
   }
@@ -45,14 +41,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
